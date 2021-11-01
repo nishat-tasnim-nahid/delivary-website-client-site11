fb0b70705ea664ad40cd8cb4891e479c9879b082
Tasnim Nahid <nnnahidt@gmail.com>
2021-11-01 20:12:32 +0600

    reset readme file

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Assignment 11:HEXA in ONE delivary site
 
-## Live site [link]
+## Live site [link](https://hexa-in-one-assignment11.web.app/home)
 
 #### If you’re running a small online business, you might need a courier service to deliver your parcels.Now, for usual courier services, you need to physically go to the courier’s office and drop the package you want to courier over.There are a handful number of such services available. To help you pick the best service in this sector, we are going to talk about 6 best online courier and delivery services in Bangladesh.
 
--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -10,7 +10,7 @@ const AboutUs = () => {
             <div className='aboutFirst'>
                 <div>
                     <h2>WHO ARE WE?</h2>
-                    <p>According to the RedSeer Report, Delhivery is the largest and fastest growing fully-integrated player in India by revenue in Fiscal 2021. We aim to build the operating system for commerce, through a combination of world-class infrastructure, logistics operations of the highest quality and cutting-edge engineering and technology capabilities.We provided supply chain solutions to a diverse base of over 21000 active customers such as e-commerce marketplaces, direct-to-consumer e-tailers and enterprises and SMEs across several verticals such as FMCG, consumer durables, consumer electronics, lifestyle, retail, automotive and manufacturing.</p>
+                    <p>According to the RedSeer Report, Delhivery is the largest and fastest growing fully-integrated player in India by revenue in Fiscal 2021. We aim to build the operating system for commerce, through a combination of world-class infrastructure, logistics operations of the highest quality and cutting-edge engineering and technology capabilities.We provided supply chain solutions to a diverse base of over 21000 active customers such as e-commerce marketplaces, direct-to-consumer e-tailers and enterprises and SMEs across several verticals such as FMCG, consumer durables, consumer electronics, lifestyle, retail, automotive and manufacturing..</p>
                 </div>
                 <div>
                     <img className='aboutImg' src="https://blog.bolt.eu/wp-content/uploads/2020/03/business-blog_1200x628-1024x536.png" alt="" />
